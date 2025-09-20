8ca67f9a7646ed4e40194069c7e63db47af591ad
Will Dougherty <doughertyw25@gmail.com>
2025-09-20 09:24:54 -0400

    style

--- src/sections/home.tsx
+++ src/sections/home.tsx
@@ -1,21 +1,25 @@
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 
 const Home = () => {
-    return (
-        <section id="home">
-            <div className="home-content">
-                <img src="/headshot.jpeg" alt="Will Dougherty" className="headshot" />
-                <div className="home-text">
-                    <h1>Will Dougherty</h1>
-                    <h2>Full-Stack Developer</h2>
-                    <div className="home-buttons">
-                        <Link to="contact" className='btn' smooth={true} duration={500}>Get In Touch</Link>
-                        <Link to="projects" className="btn" smooth={true} duration={500}>View My Work</Link>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="home">
+      <div className="home-content">
+        <img src="/headshot.jpeg" alt="Will Dougherty" className="headshot" />
+        <div className="home-text">
+          <h1>Will Dougherty</h1>
+          <h2>Full-Stack Developer</h2>
+          <div className="home-buttons">
+            <Link to="contact" className="btn" smooth={true} duration={500}>
+              Get In Touch
+            </Link>
+            <Link to="projects" className="btn" smooth={true} duration={500}>
+              View My Work
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default Home; 
+export default Home;
