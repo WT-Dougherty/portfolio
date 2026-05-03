0aeef51de7a841a3a91e12911a60c8219bdef7df
Will Dougherty <doughertyw25@gmail.com>
2026-05-03 15:04:04 -0400

    project structure laid out

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/components/home-page/home-page';
+import { ContactPage } from './pages/contact/components/contact-page/contact-page';
+import { WorkPage } from './pages/work/components/work-page/work-page';
+import { BlogPage } from './pages/blog/components/blog-page/blog-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+    title: 'Home Page',
+  },
+  {
+    path: 'work',
+    component: WorkPage,
+    title: 'My Work',
+  },
+  {
+    path: 'contact',
+    component: ContactPage,
+    title: 'Contact Me',
+  },
+  {
+    path: 'blog',
+    redirectTo: '',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/app/pages/blog/components/blog-page/blog-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-page',
+  imports: [],
+  templateUrl: './blog-page.html',
+})
+export class BlogPage {}
