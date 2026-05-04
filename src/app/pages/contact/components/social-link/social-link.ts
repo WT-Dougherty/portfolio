3b28b9513f1b8db5d6420f570e78293dbb12a2bc
Will Dougherty <doughertyw25@gmail.com>
2026-05-03 20:30:47 -0400

    contact page complete; TODO: work page

--- src/app/pages/contact/components/social-link/social-link.ts
+++ src/app/pages/contact/components/social-link/social-link.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-social-link',
   imports: [],
   templateUrl: './social-link.html',
 })
-export class SocialLink {}
+export class SocialLink {
+  icon = input.required<string>();
+  name = input.required<string>();
+  href = input.required<string>();
+}
